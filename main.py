8d711c7ec581904982f370562868292a162bce1c
korn234 <161820904+korn234@users.noreply.github.com>
2025-04-04 23:34:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -242,6 +242,16 @@ class ConfirmView(View):
                             else:
                                 print(f"❌ ไม่สามารถลบคีย์ {key} สำหรับ {self.duration} ได้")
                             video_url = "https://cdn.discordapp.com/attachments/1357266173435056169/1357385840484946122/7F0D9946-E139-4D0E-B7C8-FD67EF2825ED.mov?ex=67f00393&is=67eeb213&hm=2feb59c6e2ed4783f9be4c42e92bd0f5ec34615dc80b265adf838f969aa7681a&"
+
+                        product_embed = discord.Embed(
+                            title="🎮 รายละเอียดสินค้า",
+                            description=f"ขอบคุณสำหรับการสั่งซื้อ!\n\n"
+                            "**ตัวเกม 🎮**\n"
+                            "https://install.appcenter.ms/users/nexus2004x-gmail.com/apps/savage-ss2025/distribution_groups/2025"
+                            f"**คีย์ใช้งาน ({self.duration})**\n"
+                            f"```\n{key}\n```",
+                            color=discord.Color.gold())
+                            
                         else:
                             # Season prices
                             if key in season_keys[self.duration]:
